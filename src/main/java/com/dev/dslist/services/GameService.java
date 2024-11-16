9b8c5e75056ec940bc5a86afdb66a205fd632a65
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2024-11-16 00:46:50 -0300

    service findById created

--- src/main/java/com/dev/dslist/services/GameService.java
+++ src/main/java/com/dev/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.dev.dslist.services;
 
+import com.dev.dslist.dto.GameDto;
 import com.dev.dslist.dto.GameMinDto;
 import com.dev.dslist.entities.Game;
+import com.dev.dslist.entities.GameList;
 import com.dev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +17,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
-          List<Game> games = gameRepository.findAll(); // Lista de games
-          List<GameMinDto> gamesDto = games.stream().map( g -> new GameMinDto(g)).toList(); // transforma a lista de games para uma lista de GamesDto.
+          var games = gameRepository.findAll(); // Lista de games
+          var gamesDto = games.stream().map( g -> new GameMinDto(g)).toList(); // transforma a lista de games para uma lista de GamesDto.
           return gamesDto;
+    }
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        var game = gameRepository.findById(id).get();
+        var gameDto = new GameDto(game); // transforma o game em game dto
+        return gameDto;
     }
 }
