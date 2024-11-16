a2c8fa3dd89240e0f70655b812c07a520c30ff06
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2024-11-16 00:45:25 -0300

    endpoint findById created

--- src/main/java/com/dev/dslist/controllers/GameController.java
+++ src/main/java/com/dev/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.dev.dslist.controllers;
 
+import com.dev.dslist.dto.GameDto;
 import com.dev.dslist.dto.GameMinDto;
 import com.dev.dslist.entities.Game;
 import com.dev.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,11 @@ public class GameController {
     public List<GameMinDto> findAll(){
         List<GameMinDto> games = gameService.findAll();
         return games;
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id){
+        var gameDto = gameService.findById(id);
+        return gameDto;
     }
 }
